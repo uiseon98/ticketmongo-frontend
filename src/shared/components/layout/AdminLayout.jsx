8b9b5863bd9e017d495a5e0b7e7003d7f5a2fdf6
hsu-git <hsu080206@gmail.com>
2025-07-03 06:23:50 +0900

    fix: (admin-layout)관리자 페이지 헤더 중복 렌더링 문제 해결

--- src/shared/components/layout/AdminLayout.jsx
+++ src/shared/components/layout/AdminLayout.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './Header';
+// import Header from './Header';
 import AdminSidebar from '../../../features/admin/components/AdminSidebar';
 
 const AdminLayout = () => {
     return (
         <div className="flex flex-col min-h-screen bg-[#0A0D11]">
-            <Header /> {/* 전체 레이아웃에 헤더 포함 */}
+            {/* <Header /> */}
             <div className="flex flex-1">
-                <AdminSidebar /> {/* 관리자 사이드바 */}
+                <AdminSidebar />
                 <main className="flex-1 p-0 overflow-y-auto">
-                    <Outlet />{' '}
-                    {/* 관리자 페이지의 하위 라우트들이 렌더링될 위치 */}
+                    <Outlet />
                 </main>
             </div>
         </div>
